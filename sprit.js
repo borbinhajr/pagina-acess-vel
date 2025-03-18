53227e08c7e3234690151c9f90adde5727be06ec
Adrian Henrique de Borba <159184989+borbinhajr@users.noreply.github.com>
2025-03-18 11:19:47 -0300

    18/03

---
+++ sprit.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded',function(){
+    const aumentafonteBotao = document.getElementById('aumentar-fonte');
+    const diminuirfonteBotao = document.getElementById('diminuir-fonte');
+    let tamanhoAtualFonte= 1;
+    aumentafonteBotao. addEventListener('click',function (){
+
+    } 
+    )
+
+}
+)
